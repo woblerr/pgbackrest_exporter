860ba080f762890310db1ebc2bf4be5252f87359
woblerr <antkurochkin@gmail.com>
2022-03-12 17:47:44 +0300

    Add test for ConvertBoolToFloat64.

--- backrest/backrest_parser_test.go
+++ backrest/backrest_parser_test.go
@@ -941,3 +941,30 @@ func TestGetExporterInfoErrorsAndDebugs(t *testing.T) {
 		})
 	}
 }
+
+func TestConvertBoolToFloat64(t *testing.T) {
+	type args struct {
+		value bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{"ConvertBoolToFloat64True",
+			args{true},
+			1,
+		},
+		{"ConvertBoolToFloat64False",
+			args{false},
+			0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := convertBoolToFloat64(tt.args.value); got != tt.want {
+				t.Errorf("\nVariables do not match:\n%v\nwant:\n%v", got, tt.want)
+			}
+		})
+	}
+}
