ce43b8f03fb47ab87ead022a6bdf94c975019df8
woblerr <antkurochkin@gmail.com>
2025-09-30 23:53:26 +0300

    Disable pprof links in landing page. The pprof functionality is not enabled in the exporter.  This resulted in "404 page not found" links on the landing page.

--- backrest/backrest_exporter.go
+++ backrest/backrest_exporter.go
@@ -43,6 +43,7 @@ func StartPromEndpoint(version string, logger *slog.Logger) {
 				Description: "Prometheus exporter for pgBackRest",
 				HeaderColor: "#476b6b",
 				Version:     version,
+				Profiling:   "false",
 				Links: []web.LandingLinks{
 					{
 						Address: webEndpoint,
