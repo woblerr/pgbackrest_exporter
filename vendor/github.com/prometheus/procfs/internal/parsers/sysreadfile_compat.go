cb920ce49572fb499f70be38533f8450007ad72e
woblerr <antkurochkin@gmail.com>
2026-09-24 22:14:52 +0300

    Bump Prometheus dependencies.
    Bump client_golang from v1.23.2 to v1.24.1.
    Bump common from v0.67.5 to v0.71.0.
    Bump exporter-toolkit from v0.15.1 to v0.19.0.
    Bump procfs from v0.16.1 to v0.22.0.
    Refresh transitive dependencies and vendored sources.

--- vendor/github.com/prometheus/procfs/internal/parsers/sysreadfile_compat.go
+++ vendor/github.com/prometheus/procfs/internal/parsers/sysreadfile_compat.go
@@ -1,10 +1,9 @@
-// Copyright 2015 CoreOS, Inc.
-//
+// Copyright The Prometheus Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,10 +11,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package activation
+//go:build (linux && appengine) || (!linux && !darwin)
 
-import "os"
+package parsers
 
-func Files(unsetEnv bool) []*os.File {
-	return nil
+import (
+	"fmt"
+)
+
+// SysReadFile is here implemented as a noop for builds that do not support
+// the read syscall. For example Windows, or Linux on Google App Engine.
+func SysReadFile(file string) (string, error) {
+	return "", fmt.Errorf("not supported on this platform")
 }
